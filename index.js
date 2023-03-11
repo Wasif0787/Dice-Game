b21dbd3174be48af06d3644a067229d814c5cf06
Wasif Hussain <95441107+Wasif0787@users.noreply.github.com>
2023-03-11 16:37:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+document.querySelectorAll(".refresh")[0].addEventListener("click", function () {
+    var className=this.className;
+    makeSound();
+    buttonAnimation(className);
+});
+function makeSound() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+    var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+    var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
+    var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource);
+    image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").textContent = "Player 2 Wins!";
-} else {
-    document.querySelector("h1").textContent = "Draw";
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "Player 1 Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").textContent = "Player 2 Wins!";
+    } else {
+        document.querySelector("h1").textContent = "Draw";
+    }
+}
+
+
+
+
+function buttonAnimation(key) {
+    var elem = document.querySelector("."+key);
+    elem.classList.add("pressed");
+    setTimeout(function () {
+        elem.classList.remove("pressed");
+    }, 100);
 }
